fbcdf8a4ec9c3d3014fb81fd27a945f4b0c3c661
cottjr <carl.ott.jr@gmail.com>
2019-04-30 20:13:03 -0500

    fix dead reference to deleted bad idea library

--- club_robot_nav1/motorTasks.h
+++ club_robot_nav1/motorTasks.h
@@ -18,7 +18,6 @@
 #include <log.h>
 #include <sysclock.h>
 
-#include "commonTypes.h"
 #include "motor_funcs.h"      // provides access to motor motor drivers.  needs also helpful_defines.h     // ToDo, add #include helpful_defines.h to motor_funcs.h due to it's obvious dependency?
 #include <PID_v1.h>
 #include "nav_funcs.h"        // provides access encoder functions
